db7b531bb925a39aa98f95f490696bedf8a5b169
Christopher Negrete <55224428+chrisnnegrete@users.noreply.github.com>
2019-10-05 12:07:23 -0700

    Create bigger-margins.js

---
+++ bigger-margins.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  //frameRate(1);
+  for (var x = 98; x < width-98; x = x + 10) {
+    line(x, height/2, mouseX, mouseY);
+  }
+}
